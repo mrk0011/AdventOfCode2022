f8b454b1a55a1c8b1da6bd5eded44b92123c7e72
mrk0011 <marek.jarno.mueller@gmail.com>
2022-12-11 20:56:50 +0100

    first commit. Code does not work

--- Tag01/src/Tag1.java
+++ Tag01/src/Tag1.java
@@ -1,9 +1,29 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Tag1 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
 
+		File myFile = new File("input.txt");
+		Scanner sc = new Scanner(myFile);
+
+		int calories = 0, maxCalories = 0;
+
+		while (sc.hasNextInt()) {
+			calories = calories + sc.nextInt();
+			if (sc.nextLine().isEmpty()) {
+				if (calories > maxCalories) {
+					maxCalories = calories;
+					calories = 0;
+				}
+			}
+			
+			System.out.println(maxCalories);
+
+		}
+
+		System.out.println(maxCalories);
 	}
-
 }
